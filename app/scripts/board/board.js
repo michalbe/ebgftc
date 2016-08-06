f8e75f5f6b0c8bf4b7954d9ebc87de5b411daf8b
Michał Budzyński <michal@virtualdesign.pl>
2016-08-06 13:47:02 +0200

    baord colors

--- app/scripts/board/board.js
+++ app/scripts/board/board.js
@@ -7,13 +7,13 @@ define([
   'use strict';
 
   var rows = 5;
-  var cols = 10;
+  var cols = 11;
   var _template = _.template(template);
 
   var a = _template({
     rows: rows,
     cols: cols
   });
-  
+
   return a;
 });
