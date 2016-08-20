fb1e41df612fa149b7edadcd300a715a763974e4
Michał Budzyński <michal@virtualdesign.pl>
2016-08-20 20:33:36 +0200

    :expressionless: spawn bulb

--- app/modules/heroes/electrician.js
+++ app/modules/heroes/electrician.js
@@ -6,6 +6,32 @@ HEROES.Electrician = function() {
   this.name = 'Electrician';
 
   this.cost = 1;
+  this.attackRange = 5;
+  this.attackPower = 1;
+  this.hp = 2;
+
+  this.attack = function(cb) {
+    var self = this;
+    var tempX = this.position.x;
+    var affectedEnemies = UTILS.getUnitsHorizontalyInRange(this);
+
+    if (affectedEnemies.length > 0) {
+      var enemy = affectedEnemies[0];
+      var projectile = new PROJECTILES.Bulb();
+      console.log(this.position);
+      projectile.init(this.position.x, this.position.y);
+      projectile.render();
+      // this.moveTo(enemy.position.x, enemy.position.y, function() {
+      //   enemy.getWound(self.attackPower, function() {
+      //     self.moveTo(tempX, self.position.y);
+      //     if (typeof cb === 'function') {
+      //       cb();
+      //     }
+      //   });
+      // });
+    }
+  };
+
   return this;
 };
 
