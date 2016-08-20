165a7c18ef6e8de78bd658b52d2a58f49e9db6a0
Michał Budzyński <michal@virtualdesign.pl>
2016-08-20 20:33:52 +0200

    :rage4: change default hero to electician for debug

--- app/modules/main/main.js
+++ app/modules/main/main.js
@@ -8,7 +8,7 @@ var GAME = function() {
   GAME.units.greens = [];
   // hero players
 
-  var DefaultHero = HEROES.ConstructionWorker;
+  var DefaultHero = HEROES.Electrician;//HEROES.ConstructionWorker;
 
   var filledRows = Math.min(BOARD.rows, Math.ceil(BOARD.rows/2));
   var filledCols = 2;
