992283e4a1a8cb4a116da4ca638c6e6cba81c7ec
Michał Budzyński <michal@virtualdesign.pl>
2016-08-27 15:17:00 +0100

    :railway_car: reformat plumbers special text

--- app/modules/heroes/plumber.js
+++ app/modules/heroes/plumber.js
@@ -8,7 +8,7 @@ HEROES.Plumber = function() {
   this.cost = 4;
   this.attackRange = 10;
   this.attackPower = 1;
-  this.special = '<br/>Reverse enemy\'s row order';
+  this.special = '<br/>Reverse enemy\'s<br/>row order';
   // this.rechargeTime = 3;
   // this.hp = 1;
 
