c870517b7ebd2fbcb602da75b2c7ef10a2dc5254
Michał Budzyński <michal@virtualdesign.pl>
2016-08-28 18:48:40 +0200

    :fireworks: tailors action

--- app/modules/heroes/tailor.js
+++ app/modules/heroes/tailor.js
@@ -4,6 +4,17 @@ HEROES.Tailor = function() {
   HEROES.BasicHero.call(this);
   this.sprite = gfxMAP.tailor;
   this.name = 'Tailor';
+  this.special = 'Change same<br/>row hero\'s VP to 0';
+
+  this.attack = function(cb) {
+    var affectedUnits = UTILS.getUnitsHorizontaly(this.position.y);
+
+    UTILS.chooseUnits(affectedUnits, function(unit) {
+      UTILS.shake();
+      unit.changeVp(unit.vp*-1);
+      cb();
+    });
+  };
 
   return this;
 };
