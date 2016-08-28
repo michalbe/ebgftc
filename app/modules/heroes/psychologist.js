33d5b2d51c2096e77e054891f3ff6114b3680aa6
Michał Budzyński <michal@virtualdesign.pl>
2016-08-28 18:28:21 +0200

    :japanese_goblin: psyho action

--- app/modules/heroes/psychologist.js
+++ app/modules/heroes/psychologist.js
@@ -5,7 +5,23 @@ HEROES.Psychologist = function() {
   this.sprite = gfxMAP.psyho;
   this.name = 'Psychologist';
 
-  this.cost = 5;
+  this.attack = function(cb) {
+    var self = this;
+    var affectedUnits = UTILS.getUnitsHorizontaly(this.position.y);
+
+    affectedUnits = affectedUnits.filter(function(unit) {
+      return unit.orientation !== self.orientation;
+    });
+
+    UTILS.chooseUnits(affectedUnits, function(unit) {
+      UTILS.shake();
+      unit.rechargeStart();
+      unit.currentRechargeCount = unit.rechargeTime - 2;
+      unit.showToken('-1RE');
+      cb();
+    });
+  };
+
   return this;
 };
 
