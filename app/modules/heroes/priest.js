0832dd0246028283461e3f8e29ae50a64aa36499
Michał Budzyński <michal@virtualdesign.pl>
2016-08-28 18:28:33 +0200

    :heart_eyes: priest action

--- app/modules/heroes/priest.js
+++ app/modules/heroes/priest.js
@@ -5,6 +5,23 @@ HEROES.Priest = function() {
   this.sprite = gfxMAP.priest;
   this.name = 'Priest';
 
+  this.attack = function(cb) {
+    var self = this;
+    var affectedUnits = UTILS.getUnitsHorizontaly(this.position.y);
+
+    affectedUnits = affectedUnits.filter(function(unit) {
+      return unit.orientation !== self.orientation && !unit.priested;
+    });
+
+    UTILS.chooseUnits(affectedUnits, function(unit) {
+      UTILS.shake();
+      var vp = unit.vp*-2;
+      unit.changeVp(vp);
+      unit.priested = true;
+      cb();
+    });
+  };
+
   return this;
 };
 
